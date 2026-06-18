9d38c36c9fa00db589c1fa34150d6cab1b020b25
Coder7-dev <2847530@rambler.ru>
2026-06-18 10:06:20 +0300

    second commit

--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function loader() {
     const loader = document.querySelector('#loader');
 
@@ -10,17 +9,4 @@ function loader() {
     });
 }
 
-=======
-function loader() {
-    const loader = document.querySelector('#loader');
-
-    window.addEventListener('load', () => { 
-        setTimeout(() => {
-            loader.classList.add('loader-wrapper--hidden');
-        }, 1000)
-        
-    });
-}
-
->>>>>>> 7127bd9d5c1279a114cdf4c921f1c53ed9b0a4c2
 export default loader;
